719e05eff6237c3e4b0143cecd99e1b392341f4a
mhsngr <markus.heisinger@gmail.com>
2021-05-12 13:31:53 +0200

    add authentication to backend

--- app.js
+++ app.js
@@ -14,6 +14,67 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require("./config")(app);
 
+// session configuration
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const DB_URL = process.env.MONGODB_URI || "mongodb://localhost/paperboy";
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    saveUninitialized: false,
+    resave: true,
+    store: MongoStore.create({
+      mongoUrl: DB_URL
+    })
+  })
+)
+// end of session configuration
+
+// passport configuration
+// http://www.passportjs.org/docs/configure/
+const User = require('./models/User');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+})
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(dbUser => {
+      done(null, dbUser)
+    })
+    .catch(err => {
+      done(err);
+    })
+})
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username })
+      .then(userFromDB => {
+        if (userFromDB === null) {
+          done(null, false, { message: 'Username does not exist' });
+        } else if (!bcrypt.compareSync(password, userFromDB.password)) {
+          done(null, false, { message: 'Wrong password' });
+        } else {
+          done(null, userFromDB);
+        }
+      })
+      .catch(err => {
+        next(err);
+      })
+  })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
+// end of passport configuration
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controled from the routes/index.js
 // const allRoutes = require("./routes");
@@ -21,6 +82,9 @@ require("./config")(app);
 
 const feeds = require("./routes/feeds");
 app.use("/api/feeds", feeds);
+
+const auth = require("./routes/auth");
+app.use("/api/auth", auth);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
